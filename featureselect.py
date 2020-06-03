db4956712f8d26604a49e2d7c1dc698758bd9903
Justin Gafford <jgaff001@ucr.edu>
2020-06-03 13:15:11 -0700

    Added Several functions, updated main
    Implemented main and all function necessary to read in and normalize our data, now all that's really needed to implement is our actual search algorithms(1-3), thus I am assuming that my NormData is correct as I will not know until I implement my algorithms

--- featureselect.py
+++ featureselect.py
@@ -7,8 +7,7 @@ def PopulateHeap(data, instanceCount, featureCount, allFeatures):
     
     for i in range(1, featureCount + 1):
         if (i not in allFeatures):
-            accuracy = CrossValidation(data, instanceCount, \
-                allFeatures, i)
+            accuracy = CrossValidation(data, instanceCount, allFeatures, i)
             featureMatch = (1 - accuracy, i)
             heapq.heappush(TheQueue, featureMatch)
 
@@ -18,7 +17,7 @@ def PopulateHeap(data, instanceCount, featureCount, allFeatures):
 def AddFeatureToHeap(data, instanceCount, featureCount, allFeatures, TheQueue):
     first = heapq.heappop(TheQueue)
     newFeature = first[1]
-    accuracy = CrossValidation(data, instanceCount, \ allFeatures, newFeature)
+    accuracy = CrossValidation(data, instanceCount, allFeatures, newFeature)
     return newFeature, accuracy
 
 # uses one out k-fold cross validation in order to predict the accuracy of our data
@@ -74,7 +73,73 @@ def Classification(instances, nn, removeOne):
         return False
     return True
 
+#here we read in the data from the input file(database file) and return the instances
+def ReadData(filename, instanceCount):
+    try:
+        file = open(filename, 'r')
+    except:
+        raise FileNotFoundError(filename)
+    instances = [[] for i in range(instanceCount)]
+    
+    for i in range(instanceCount):
+        instances[i] = [float(j) for j in file.readline().split()]
+        
+    return instances
+#here we normalize the data given from the text file, normInstances is temp value(makes sure we dont update instances
+def NormData(instances, instanceCount, featureCount):
+    normInstances = list(instances)
+    mean = CalcMean(instances, instanceCount, featureCount)
+    std = CalcStd(instances, instanceCount, featureCount, mean)
+    
+    for i in range(0, instanceCount):
+        for j in range(1, featureCount + 1):
+            normInstances[i][j] = ((instances[i][j] - mean[j - 1]) / std[j - 1])
+
+    return normInstances
+
+#calculates the average used in our normdata def
+def CalcMean(instances, instanceCount, featureCount):
+    mean = []
+    
+    for i in range(1, featureCount + 1): # Add one to exclude the class data
+        mean.append((sum(row[i] for row in instances)) / instanceCount)
+        
+    return mean
+
+#calcualtes the standard deviation for our normdata def
+def CalcStd(instances, instanceCount, featureCount, mean):
+    std = []
+    
+    for i in range(1, featureCount + 1):
+        std.append(math.sqrt((sum(pow((row[i] - mean[i - 1]), 2) for row in instances)) / instanceCount))
+        
+    return std
+
 def main():
+    print("Welcome to Justin Gaffords Feature Selection algorithm")
+    filename = input("Type in the name of the file to test:")
+    instanceCount = int(input("\nEnter the number of instances to read:"))
+    instances = ReadData(filename, instanceCount)
+    featureCount = len(instances[0]) - 1
+    print("\nThis dataset has", featureCount, "features, with",\
+        instanceCount, "instances")
+    print("\nPlease wait while I normalize the data... Done!")
+    normInstances = NormData(instances, instanceCount, featureCount)
+    algorithmChoice = ""
+    
+    while (algorithmChoice != "1" and algorithmChoice != "2" and algorithmChoice != "3"):
+        algorithmChoice = input("""\nType in the number of the algorithm you want to run:\n
+                       1) Forward Selection
+                       2) Backward Elimination
+                       3) Custom Search
+                    \r""")
+    
+    if (algorithmChoice == "1"):
+        ForwardSelection(normInstances, instanceCount, featureCount)
+    elif (algorithmChoice == "2"):
+        BackwardElimination(normInstances, instanceCount, featureCount)
+    else:
+        CustomSearch(normInstances, instanceCount, featureCount)
     
 if __name__ == "__main__":
     main()
